e872fe7f365931b43dd73cd807ddf27550196ea9
zettacamp-muhammad-syaiful <syaiful.karim@zettacamp.pro>
2022-04-15 02:29:52 +0700

    Styling Halaman Create Box

--- src/pages/CreateBlog/index.js
+++ src/pages/CreateBlog/index.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Button, Gap, Input, TextArea, Upload } from "../../components";
+import "./CreateBlog.scss";
 
 const CreateBlog = () => {
-  return <div>Conten Create Blog Page</div>;
+  return (
+    <div className="blog-post">
+      <p className="title">Create new Blog</p>
+      <Input title="Post Title" />
+      <Upload />
+      <TextArea />
+      <Gap height={20} />
+      <div className="button-action">
+        <Button title="Save" />
+      </div>
+      <Gap height={30} />
+    </div>
+  );
 };
 
 export default CreateBlog;
