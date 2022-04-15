e1f88885b48f87916cb91cfbac7d8be7655adc33
syaiful505 <syaifulstyle505@gmail.com>
2022-04-15 14:21:19 +0700

    styling halaman detail blog

--- src/pages/DetailBlog/index.js
+++ src/pages/DetailBlog/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { RegisterBg } from '../../assets' ;
+import './detailblog.scss';
 
 const DetailBlog = () => {
-  return <div>Detail Blog Page</div>;
+  return (
+    <div className="detail-blog-wrapper">
+      <img className="img-cover" src={RegisterBg} alt="thumb" />
+      <p className="blog-title">Title Blog</p>
+      <p className="blog-author">Author - Date Post</p>
+      <p className="blog-body">Ini adalah detail Blog Page</p>
+    </div>
+  )
 };
 
 export default DetailBlog;
